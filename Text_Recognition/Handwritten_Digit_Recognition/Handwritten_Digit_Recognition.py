0b189ae5ec29c0be1e5a597d195b1c4e606f278a
GuanYu Lin <lgy3030@gmail.com>
2020-09-03 20:19:24 +0800

    use neural network to solve Handwritten Digit Recognition

--- Text_Recognition/Handwritten_Digit_Recognition/Handwritten_Digit_Recognition.py
+++ Text_Recognition/Handwritten_Digit_Recognition/Handwritten_Digit_Recognition.py
@@ -1,52 +1,51 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
-from sklearn.datasets import fetch_openml
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = tf.keras.utils.normalize(X_train, axis=1)
+X_test = tf.keras.utils.normalize(X_test, axis=1)
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
 
+# Compiling and optimizing model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# In[3]:
+# Training the model
+model.fit(X_train, y_train, epochs=3)
 
-
-mnist = fetch_openml('mnist_784')
+val_loss, val_acc = model.evaluate(X_test, y_test)
+print(val_loss)
+print(val_acc)
 
 
 # In[5]:
 
 
-x, y = mnist['data'], mnist['target']
+import matplotlib.pyplot as plt
+def plot_loss(history, label, n):
+  # Use a log scale to show the wide range of values.
+  plt.semilogy(history.epoch,  history.history['loss'],
+               color=colors[n], label='Train '+label)
+  plt.semilogy(history.epoch,  history.history['val_loss'],
+          color=colors[n], label='Val '+label,
+          linestyle="--")
+  plt.xlabel('Epoch')
+  plt.ylabel('Loss')
+  
+  plt.legend()
 
 
 # In[6]:
 
 
-get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[7]:
-
-
-import matplotlib
-import matplotlib.pyplot as plt
-
-
-# In[8]:
-
-
-some_digit = x[2500]
-some_digit_image = some_digit.reshape(28,28)
-
-
-# In[10]:
-
-
-plt.imshow(some_digit_image, cmap = matplotlib.cm.binary, interpolation = 'nearest')
-
-
-# In[ ]:
-
-
-
+plot_loss(model, "Zero Bias", 0)
 
